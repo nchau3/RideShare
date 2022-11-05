c2a5481279290640d339f0f0e808763847ebdfbf
Nicholas <nc_chau@hotmail.com>
2022-11-05 15:41:58 -0400

    reorganize styles directory

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "../styles/page-styles/register.scss";
 
 export default function Register(props) {
   const [firstName, setFirstName] = useState("");
