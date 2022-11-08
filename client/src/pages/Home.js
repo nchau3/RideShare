c2c975a1fc0209f9ce660d4292f123c57752d96c
Coreen Huang <coreen.huang@gmail.com>
2022-11-07 20:21:23 -0500

    add content to homepage and button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,12 @@ import React from "react";
 export default function Home() {
   return (
     <div className="page-container">
-      <h1>Homepage!</h1>
+      <h1> Welcome to RoundTrip</h1>
+      <h2> • Travel to your destination </h2>
+      <h2> • Connecting you to available rides </h2>
+      <h2> • Cashless payment method </h2>
+      <h2> • Environmentally-friendly </h2>
+      <button> Book a Trip Now! </button>
     </div>
   );
 }
